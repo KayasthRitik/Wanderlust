3821d51d81362f918d80eeb32815a11f40063c73
Ritik Srivastav <ritiksrivastav2102@gmail.com>
2024-07-22 12:15:14 +0530

    updated init folder

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "66961820bdb1655ba161bb5c" }))
     await Listing.insertMany(initData.data);
     console.log("data was initialised");
 }
